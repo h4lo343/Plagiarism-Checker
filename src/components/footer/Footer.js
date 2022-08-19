05dcc83c4eecbb380f36493b29e93180920c2479
zhuocun <zhuocun.tang@outlook.com>
2022-08-19 19:27:18 +1000

    update project architecture

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react"
+import styles from "./Footer.module.css"
+import {Layout} from "antd";
+
+const { Footer: PageFooter } = Layout;
+
+export const Footer = () => {
+    return (
+        <PageFooter className={styles.footer}>
+            Ant Design ©2018 Created by Ant UED
+        </PageFooter>
+    )
+};
