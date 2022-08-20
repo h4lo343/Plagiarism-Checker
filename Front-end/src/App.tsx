c290bbcd5586375a794ad0902d953c1523d8cb18
zhuocun <zhuocun.tang@outlook.com>
2022-08-21 02:17:19 +1000

    improve format and manage repository

--- Front-end/src/App.tsx
+++ Front-end/src/App.tsx
@@ -4,13 +4,13 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 const App = () => {
   return (
       <div>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="*" element={<h1>404 not found</h1>} />
-          </Routes>
+          <BrowserRouter>
+              <Routes>
+                  <Route path="/" element={<HomePage />} />
+                  <Route path="/login" element={<Login />} />
+                  <Route path="/signup" element={<Signup />} />
+                  <Route path="*" element={<h1>404 not found</h1>} />
+              </Routes>
         </BrowserRouter>
       </div>
   );
