df172df7c2afb35fec74afef885c06267533ff63
zhuocun <zhuocun.tang@outlook.com>
2022-08-26 23:15:05 +1000

    merge

--- Front-end/src/components/header/Header.tsx
+++ Front-end/src/components/header/Header.tsx
@@ -1,17 +1,26 @@
-import React from "react"
-import {Divider, Typography} from "antd";
+import React, { useEffect, useState } from "react"
+import antdStyle from "../../App.css";
+import {Divider, Layout, Typography} from "antd";
 import styles from "./Header.module.css";
+import Pubsub from 'pubsub-js'
 
-interface PropType {
-  headerName: string
+const {Header: PageHeader} = Layout;
+
+interface Propstype {
+    headerName: string
 }
 
-export const Header:React.FC<PropType> = ({headerName}) => {
+export const Header:React.FC<Propstype> = () => {
+    const [title, setTitle] = useState("")
+    Pubsub.subscribe("title",(_:any,data:string)=>{
+        setTitle(data)
+    })
     return (
         <div className={styles['header']}>
-          <Divider orientation="center">
-            <Typography.Title  level={2}>{headerName} page</Typography.Title>
-          </Divider>
+            <Divider orientation="center">
+                <Typography.Title  level={2}>{title} page</Typography.Title>
+            </Divider>
         </div>
+
     );
 };
