0ba8a88b84c56437f3bce46cc36129b1da1cecf3
zhuocun <zhuocun.tang@outlook.com>
2022-08-27 14:58:09 +1000

    merge

--- Front-end/src/pages/login/LoginPage.tsx
+++ Front-end/src/pages/login/LoginPage.tsx
@@ -1,25 +1,49 @@
-import {Button, Checkbox, Form, Input} from 'antd';
-import React from 'react';
-import styles from "./LoginPage.module.css"
-import {Watermark} from '../../components';
-import {Link, useNavigate} from 'react-router-dom';
-import {BackgroundImage} from '../../components';
+import {Link, useNavigate} from "react-router-dom";
+import {Button, Checkbox, Form, Input} from "antd";
+import {BackgroundImage} from "../../components";
+import styles from "./LoginForm.module.css";
+import {Watermark} from "../../components";
+import React, {useEffect} from "react";
+import {useReduxDispatch, useReduxSelector} from "../../redux/hooks";
+import {login} from "../../redux/authentication/slice";
 
-export const LoginPage: React.FC = () => {
 
-    const navigate = useNavigate()
-    const [form] = Form.useForm()
+export const LoginForm = () => {
+    /*
+        const loading = useReduxSelector((s) => s.authentication.loading);
+        const jwtToken = useReduxSelector((s) => s.authentication.jwtToken);
 
-    const login = () => {
+        const dispatch = useReduxDispatch();
+
+        useEffect(() => {
+            if (jwtToken !== null) {
+                navigate("/");
+            }
+        }, [navigate, jwtToken])
+    */
+
+    const loading = false;
+    const navigate = useNavigate();
+    const [form] = Form.useForm();
+
+    const mockLogin = () => {
         const username = form.getFieldValue("username")
         const password = form.getFieldValue("password")
-        if (password === "123" && username === "123") {
+        if (password === "123" && username === "student") {
+            navigate('/student')
+        } else if (password === "123" && username === "teacher") {
             navigate('/teacher')
         }
     }
 
     const onFinish = (values: any) => {
         console.log('Success:', values);
+        /*
+        dispatch(login({
+            username: values.username,
+            password: values.password
+        }))
+        */
     };
 
     const onFinishFailed = (errorInfo: any) => {
@@ -31,20 +55,20 @@ export const LoginPage: React.FC = () => {
             <BackgroundImage/>
             <Form
                 name="basic"
-                labelCol={{span: 8}}
-                wrapperCol={{span: 16}}
+                labelCol={{span: 6}}
+                wrapperCol={{span: 18}}
                 initialValues={{remember: true}}
                 onFinish={onFinish}
                 onFinishFailed={onFinishFailed}
                 autoComplete="off"
-                className={styles["register-form"]}
+                className={styles["login-form"]}
                 form={form}
             >
                 <Form.Item
                     label="Username"
                     name="username"
                     rules={[{required: true, message: 'Please input your username!'}]}
-                    style={{marginTop: "10px"}}
+                    style={{marginTop: "80px"}}
                 >
                     <Input className={styles['input-box']}/>
                 </Form.Item>
@@ -62,17 +86,15 @@ export const LoginPage: React.FC = () => {
                 </Form.Item>
 
                 <Form.Item wrapperCol={{offset: 8, span: 16}}>
-                    <Button type="primary" htmlType="submit" onClick={login}>
+                    <Button type="primary" htmlType="submit" onClick={mockLogin} loading={loading}>
                         Submit
                     </Button>
 
                     <Link to="/register" style={{marginLeft: "10px"}}>Register for an account</Link>
 
                 </Form.Item>
-                <Watermark top='29%' left='55%'/>
+                <Watermark top='24%' left='68%'/>
             </Form>
         </div>
-
-
-    );
+    )
 };
