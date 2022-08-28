e29d46e52e458b73ff680c33073e704a139531f4
Zixuan Liu <zixliu@student.unimelb.edu.au>
2022-08-28 22:33:42 +1000

    Updated apis for EA demo

--- Back-end/controllers/fileController.js
+++ Back-end/controllers/fileController.js
@@ -2,7 +2,6 @@ const pdfParse = require('pdf-parse')
 const fs = require('fs')
 const path = require('path')
 
-let fileName
 const postSinglePDF = async(req, res) => {
     console.log("postSinglePDF")
     try{
@@ -19,7 +18,8 @@ const postSinglePDF = async(req, res) => {
                             console.error(err);
                         }
                         else {
-                            res.status(200).send(result.text);
+                            api_stub('mockRate');
+                            res.status(200).send("File uploaded");
                         }
                     })
                 })
@@ -36,8 +36,9 @@ const postSinglePDF = async(req, res) => {
 const getSingleTXT = async(req, res) => {
     console.log("getSingleTXT")
     try{
-        console.log(`./result/${path.parse(fileName).name}.txt`)
-        let file = `./result/${path.parse(fileName).name}.txt`;
+        // console.log(`./result/${path.parse(fileName).name}.txt`)
+        // let file = `./result/${path.parse(fileName).name}.txt`;
+        let file = './result/risk.txt';
         res.download(file);
     } catch(err) {
         console.log(err);
@@ -45,8 +46,41 @@ const getSingleTXT = async(req, res) => {
     }
 }
 
+const getMockResult = async(req, res) => {
+    console.log("getMockResult")
+    try{
+        fs.readFile('./result/mockRate.txt', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            console.log(data);
+            result = {
+                submissionID: 2,
+                submitter: "Bob",
+                file: "56785.pdf",
+                uploadTime: new Date(2022, 4, 12).toDateString(),
+                similarity: data,
+                PorF: "pass"
+            }
+            res.status(200).send(result);
+        });
+    } catch(err) {
+        console.log(err);
+        res.status(500).send(err);
+    }
+}
+
+function api_stub(fileName) {
+    fs.writeFile(`./result/${path.parse(fileName).name}.txt`, '10%', function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+    });
+}
+
 
 module.exports = {
     postSinglePDF,
-    getSingleTXT
+    getSingleTXT,
+    getMockResult
 }
--- Back-end/routes/fileRouter.js
+++ Back-end/routes/fileRouter.js
@@ -4,5 +4,6 @@ const fileController = require('../controllers/fileController');
 fileRouter.post("/post-one-pdf", fileController.postSinglePDF);
 
 fileRouter.get("/get-one-txt", fileController.getSingleTXT);
+fileRouter.get("/get-mock-result", fileController.getMockResult);
 
 module.exports = fileRouter;
