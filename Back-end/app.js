ca3fb3c1bd634b496b59f7506454051c24bd191c
Negronii <478258625@qq.com>
2022-09-11 16:09:06 +1000

    user authentication and connect DB

--- Back-end/app.js
+++ Back-end/app.js
@@ -1,9 +1,16 @@
 const express = require('express');
-// const mongoose = require('mongoose');
+const authRouter = require('./routes/authRouter');
+const verifyToken = require('./middlewares/verifyToken').verifyToken;
 const cors = require('cors');
 const fileUpload = require("express-fileupload");
+const fileRouter = require("./routes/fileRouter");
+const { default: mongoose } = require('mongoose');
+const connectDB = require('./Config/connectMongo');
 require('dotenv').config();
 // console.log(process.env)
+
+//connect to database
+connectDB();
 
 const app = express();
 app.use(cors());
@@ -12,18 +19,22 @@ app.use(fileUpload());
 
 const port = process.env.PORT || 8888;
 
-const fileRouter = require("./routes/fileRouter.js");
-
 app.get("/", (req, res) => {
     res.send("SC-Quokka presents");
 });
 
+app.use("/auth", authRouter);
+app.use(verifyToken);
 app.use("/file", fileRouter);
 
-app.listen(port, (error) =>{
-    if(!error)
-        console.log("Server is Successfully Running, and App is listening on port "+ port);
-    else 
-        console.log("Error occurred, server can't start", error);
+mongoose.connection.once('open', () => {
+    console.log("Connected to MongoDB");
+    app.listen(port, (error) => {
+        if (!error)
+            console.log("Server is Successfully Running, and App is listening on port " + port);
+        else
+            console.log("Error occurred, server can't start", error);
     }
-);
+    );
+});
+
---
+++ Back-end/models/user.js
@@ -0,0 +1,29 @@
+// require mongoose module
+const mongoose = require("mongoose")
+const subject = require('./subject')
+
+/* -------------------------------------- MODEL -------------------------------------- */
+const userSchema = new mongoose.Schema({
+
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    subjects: [
+        {subject: subject}
+    ]
+})
+
+
+const User = mongoose.model("User", userSchema)
+
+module.exports = User
