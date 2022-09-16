abff58ad9e0d27054a9eb2d5a3a801539bba2d2a
Dennis <honor1016dd@gmail.com>
2022-09-16 19:20:07 +1000

    Update subjectController.js

--- Back-end/controllers/subjectController.js
+++ Back-end/controllers/subjectController.js
@@ -15,6 +15,7 @@ const createSubject = async(req, res) => {
 
         const subjectCode = req.body.subjectCode
         const teacherEmail = req.body.teacherEmail
+        // console.log(teacherEmail)
         const teacher = await User.findOne({email: teacherEmail, role: "teacher"});
         if(!teacher) res.status(409).json({msg: "Teacher not found"});
         if(!subjectCode) return res.status(409).json({msg: "No subjectCode!"});
@@ -25,7 +26,8 @@ const createSubject = async(req, res) => {
             subjectName: subjectName,
             teachers: teacher._id 
         })
-        const result = await User.updateOne({userEmail: teacherEmail}, {$push: {subjects: newSubject._id}})
+        // console.log(newSubject._id)
+        const result = await User.updateOne({email: teacherEmail}, {$push: {subjects: newSubject._id}})
         newSubject.save()
         return res.status(200).json({msg: "Create subject successfully"})
 
@@ -55,7 +57,7 @@ const deleteSubject = async(req, res) => {
         if(!subject) return res.status(409).json({ msg: "Subject not found"});
         if(!await User.findOne({email: userEmail, subjects: subject._id})) return res.status(409).json({ msg: "Subject does not exist in the subjectList"}); 
         const result = await User.updateOne({email: userEmail}, {$pull: {subjects: subject._id}})
-        // const result2 = await Subject.deleteOne({subjectCode: subjectCode})
+        const result2 = await Subject.deleteOne({subjectCode: subjectCode})
         return res.status(200).json({msg: "Delete subject successfully"})
     } catch(error){
         res.status(500).json({msg: error.message})
@@ -65,10 +67,11 @@ const deleteSubject = async(req, res) => {
 const getSubjectList = async(req, res) => {
     try{
         const userEmail = req.email
+        console.log(userEmail)
         // const subjects = await User.find({email: userEmail}).populate("subjects")
         const user = await User.findOne({email: userEmail}).populate("subjects")
-        // console.log(user)
-        // console.log(user.subjects)
+        console.log(user)
+        console.log(user.subjects)
         return res.status(200).json(user.subjects)
     } catch(error){
         res.status(500).json({msg: error.message})
