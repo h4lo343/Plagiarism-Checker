8608dd36cbd4b86cda6d503e88898e3519cc9062
Dennis <honor1016dd@gmail.com>
2022-09-17 19:47:43 +1000

    Implement deleteFile function and refactor getBufferFiles

--- Back-end/routes/bufferFileRouter.js
+++ Back-end/routes/bufferFileRouter.js
@@ -2,5 +2,6 @@ const bufferFileRouter = require('express').Router();
 const bufferController = require('../controllers/bufferFileContorller');
 
 bufferFileRouter.get("/getBufferFileList", bufferController.getBufferFiles);
+bufferFileRouter.post("/deleteFile", bufferController.deleteBufferFile);
 
 module.exports = bufferFileRouter;
