91dbbd80aeb9f3a89af9516081ec7855178fb68c
h4lo343 <92515186+h4lo343@users.noreply.github.com>
2022-09-17 23:01:18 +1000

    9/17

--- Front-end/src/pages/assignmentDetail/AssignmentDetailPage.tsx
+++ Front-end/src/pages/assignmentDetail/AssignmentDetailPage.tsx
@@ -1,78 +1,98 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { AssignmentDatabaseList, UploadBox, UploadedFileList } from "../../components";
 import { useReduxDispatch, useReduxSelector } from "../../redux/hooks";
 import { getUploadedFileList } from "../../redux/uploadedFileList/slice";
 import { mockFileList, mockDatabaseList } from "./mock";
 import styles from "./AssignmentDetailPage.module.css";
-import { Button, Col, Row, Upload } from "antd";
+import { Button, Col, Row, Upload, Select } from "antd";
 import { getResult } from "../../redux/result/slice";
 import { BufferFileList } from "../../components/bufferFileList/BufferFileList";
 import { getBufferFileList } from "../../redux/bufferFileList/slice";
 import { HttpRequestHeader } from "antd/es/upload/interface";
+import axios from "axios";
 
 export const AssignmentDetailPage: React.FC = () => {
-    const navigate = useNavigate();
+  const { Option } = Select;
+  const navigate = useNavigate();
 
-    const jwtToken = useReduxSelector((s) => s.authentication.jwtToken) as string;
-    const uploadedFileList = useReduxSelector((s) => s.uploadedFileList.fileList);
+  const jwtToken = useReduxSelector((s) => s.authentication.jwtToken) as string;
+  const uploadedFileList = useReduxSelector((s) => s.uploadedFileList.fileList);
 
-    const resultLoading = useReduxSelector((s) => s.result.loading);
-    const bufferFileLoading = useReduxSelector((s) => s.bufferFileList.loading);
-    const bufferFileList = useReduxSelector((s) => s.bufferFileList.bufferFileList);
-    //const resultDetail = useReduxSelector((s) => s.result.resultDetail);
-    const { subjectCode, assignmentName } = useParams();
-    console.log(subjectCode, assignmentName);
-    const dispatch = useReduxDispatch();
-    useEffect(() => {
-        PubSub.publish("title", assignmentName);
-        if (jwtToken) {
-            dispatch(getBufferFileList({ jwtToken, subjectCode, assignmentName }));
+  const resultLoading = useReduxSelector((s) => s.result.loading);
+  const bufferFileLoading = useReduxSelector((s) => s.bufferFileList.loading);
+  const bufferFileList = useReduxSelector((s) => s.bufferFileList.bufferFileList);
+  //const resultDetail = useReduxSelector((s) => s.result.resultDetail);
+  const { subjectCode, assignmentName } = useParams();
+  const [fileType, setFileType] = useState("")  
+
+  const dispatch = useReduxDispatch();
+  useEffect(() => {
+    PubSub.publish("title", assignmentName);
+    if (jwtToken) {
+      dispatch(getBufferFileList({ jwtToken, subjectCode, assignmentName }));
+    }
+  }, [dispatch, jwtToken]);
+
+  const onChange = (value:string) => {
+    setFileType(value)
+  }
+
+  const check = async () => {
+    const reply = await axios.post(`http://localhost:8888/check/postCheckConfig`,
+      { 
+        subjectCode: subjectCode,
+        assignment: assignmentName,
+        dataType: fileType,
+        datasets: "dataset1"
+      },
+      {
+        headers: {
+          token: jwtToken
         }
-    }, [dispatch, jwtToken]);
+      }
+    )
+  }
 
 
-    /*
-        useEffect(() => {
-            PubSub.publish("title", `Assignment-${assignmentName}`);
-            dispatch(getResult());
-        }, [assignmentName, dispatch, resultDetail]);
+  return (
+    <div>
+      <Row>
+        <Col span={9} className={styles["database-list"]}>
+          {/*<UploadedFileList uploadedFileList={resultDetail} />*/}
+          <BufferFileList loading={bufferFileLoading} bufferFileList={bufferFileList} />
+          <div className={styles.button}>
+            <UploadBox />
+          </div>
+        </Col>
+        <Col span={9} className={styles["database-list"]}>
+          <AssignmentDatabaseList
+            loading={false}
+            assignmentDatabaseList={mockDatabaseList}
+          />
+          <Select
+            style={{width:170, marginRight: 15}}
+            showSearch
+            placeholder="Select a file type to check"
+            onChange={onChange}
+            optionFilterProp="type"
+            filterOption={(input, option) =>
+              (option!.children as unknown as string).toLowerCase().includes(input.toLowerCase())
+            }
+          >
+            <Option value="pdf" key={1}>Pdf</Option>
+            <Option value="docx" key={2}>Docx</Option>
+            <Option value="java" key={3}>Java</Option>
+            <Option value="c" key={4}>C</Option>
+          </Select>
 
-     */
-
-    return (
-        <div>
-            <Row>
-                <Col span={9} className={styles["database-list"]}>
-                    {/*<UploadedFileList uploadedFileList={resultDetail} />*/}
-                    <BufferFileList loading={bufferFileLoading} bufferFileList={bufferFileList} />
-                    <div className={styles.button}>
-                        <UploadBox />
-                    </div>
-                </Col>
-                <Col span={9} className={styles["database-list"]}>
-                    <AssignmentDatabaseList
-                        loading={false}
-                        assignmentDatabaseList={mockDatabaseList}
-                    />
-                    <div className={styles.button}>
-                        <Button
-                            style={{
-                                width: 100,
-                                height: 45,
-                                fontSize: "15px",
-                                borderRadius: 10
-                            }}
-
-                            onClick={() => navigate("/student/result/detail/SCORE%20Sprint%201")}
-                        >
-                            Check
-                        </Button>
-                    </div>
-                </Col>
-            </Row>
+          <Button onClick={check}>
+            Check
+          </Button>
+        </Col>
+      </Row>
 
 
-        </div>
-    );
+    </div>
+  );
 };
